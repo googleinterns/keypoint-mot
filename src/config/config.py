29917343958491e27242224e10a1fdea040ce5b0
Vasile Lup <vasilup@google.com>
2020-10-07 10:28:21 +0000

    added more type hints

--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,5 @@
 import collections
+from typing import Any, Dict
 
 from config import paths
 
@@ -8,16 +9,16 @@ DATASET_ROOT = {'nuscenes': paths.DIR_NUSCENES}
 TRAIN_RESOLUTION = {'nuscenes': Resolution(height=448, width=800)}
 
 
-def _get_dataset_param(dataset, all_params):
+def _get_dataset_param(dataset: str, all_params: Dict[str, Any]):
     if dataset in all_params:
         return all_params[dataset]
 
     raise ValueError(f'Unknown dataset: {dataset}')
 
 
-def get_data_dir(dataset):
+def get_data_dir(dataset: str) -> str:
     return _get_dataset_param(dataset, DATASET_ROOT)
 
 
-def get_train_resolution(dataset):
+def get_train_resolution(dataset: str) -> Resolution:
     return _get_dataset_param(dataset, TRAIN_RESOLUTION)
--- src/dataset/dataset_factory.py
+++ src/dataset/dataset_factory.py
@@ -4,6 +4,6 @@ from dataset import nuscenes_dataset
 DATASETS = {'nuscenes': nuscenes_dataset.NuscenesDataset}
 
 
-def get_dataset(dataset_name, subset, mini_version: bool):
+def get_dataset(dataset_name: str, subset: str, mini_version: bool):
     return DATASETS[dataset_name](subset=subset, dataset_root=config.get_data_dir(dataset_name),
                                   mini_version=mini_version)

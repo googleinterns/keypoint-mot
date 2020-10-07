4bc21e5c225517113c5ac5a55763431ed9208dc0
Vasile Lup <vasilup@google.com>
2020-10-07 10:28:21 +0000

    added more type hints

--- src/dataset/nuscenes_dataset.py
+++ src/dataset/nuscenes_dataset.py
@@ -3,14 +3,17 @@ from nuscenes.utils import splits
 
 
 class NuscenesDataset:
+    """
+    Handles loading and splitting of Nuscenes dataset
+    """
     version = '1.0'
 
-    def __init__(self, subset, dataset_root, mini_version: bool):
+    def __init__(self, subset: str, dataset_root: str, mini_version: bool):
         self.nusc = self.__get_nuscenes_instance(subset, dataset_root, mini_version)
         scene_names = splits.create_splits_scenes()[f'mini_{subset}' if mini_version else subset]
         self.scenes = [scene for scene in self.nusc.scene if scene['name'] in scene_names]
 
-    def __get_nuscenes_instance(self, subset, dataset_root, mini):
+    def __get_nuscenes_instance(self, subset, dataset_root, mini: bool):
         if subset in ['train', 'val']:
             suffix = 'trainval'
         else:

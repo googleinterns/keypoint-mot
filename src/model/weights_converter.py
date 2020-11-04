9261eff0655ba9baf7cfb6de8664cbbbe1e7f438
Vasile Lup <vasilup@google.com>
2020-11-04 09:08:58 +0000

    added specific pytorch weight handlers

--- src/model/weights_converter.py
+++ src/model/weights_converter.py
@@ -1,6 +1,9 @@
 import logging
 from typing import NamedTuple
 
+import numpy as np
+import tensorflow as tf
+
 
 class WeightHandlerReturn(NamedTuple):
     """
@@ -59,3 +62,80 @@ class WeightHandler:
     def _process_weights(self, source_weight, target_weight) -> WeightHandlerReturn:
         """Should provide the actual processing logic for the given weight pair"""
         raise NotImplementedError
+
+
+class PytorchToTensorflowHandlers:
+    @staticmethod
+    def _preprocess_tensorflow(tensorflow_weight):
+        return tensorflow_weight, tensorflow_weight.name, tuple(tensorflow_weight.shape)
+
+    @staticmethod
+    def _preprocess_pytorch(pytorch_weight):
+        return pytorch_weight[1], pytorch_weight[0], tuple(pytorch_weight[1].shape)
+
+    class SameShapeHandler(WeightHandler):
+        """Assigns source_weight to target_weight if the weights have same shape."""
+
+        def _process_weights(self, source_weight, target_weight) -> WeightHandlerReturn:
+            weight_tf, name_tf, shape_tf = PytorchToTensorflowHandlers._preprocess_tensorflow(target_weight)
+            weight_py, name_py, shape_py = PytorchToTensorflowHandlers._preprocess_pytorch(source_weight)
+
+            if shape_tf == shape_py:
+                weight_tf.assign(weight_py.numpy())
+                if self.logger:
+                    self.logger.info(f'{name_tf} was assigned successfully from {name_py}')
+                return WeightHandlerReturn(processed=True, matched_source=True, matched_target=True)
+
+            return WeightHandlerReturn(processed=False, matched_source=False, matched_target=False)
+
+    class ConvolutionHandler(WeightHandler):
+        """Assigns source_weight to target_weight if the weights are 2D convolution weights."""
+
+        def _process_weights(self, source_weight, target_weight) -> WeightHandlerReturn:
+            weight_tf, name_tf, shape_tf = PytorchToTensorflowHandlers._preprocess_tensorflow(target_weight)
+            weight_py, name_py, shape_py = PytorchToTensorflowHandlers._preprocess_pytorch(source_weight)
+
+            if len(shape_tf) == len(shape_py) == 4 and shape_tf == (shape_py[2], shape_py[3], shape_py[1], shape_py[0]):
+                weight_tf.assign(weight_py.numpy().transpose(2, 3, 1, 0))
+                if self.logger:
+                    self.logger.info(f'{name_tf} was assigned successfully from {name_py}, transposed')
+                return WeightHandlerReturn(processed=True, matched_source=True, matched_target=True)
+
+            return WeightHandlerReturn(processed=False, matched_source=False, matched_target=False)
+
+    class DepthwiseTransposedConvolutionHandler(WeightHandler):
+        """
+        Assigns source_weight to target_weight if the source_weight is a depthwise transposed convolution and
+        target_weight is a regular transposed convolution.
+        """
+
+        def _process_weights(self, source_weight, target_weight) -> WeightHandlerReturn:
+            weight_tf, name_tf, shape_tf = PytorchToTensorflowHandlers._preprocess_tensorflow(target_weight)
+            weight_py, name_py, shape_py = PytorchToTensorflowHandlers._preprocess_pytorch(source_weight)
+
+            if 'conv2d_transpose' in name_tf and shape_tf[:2] == shape_py[2:] and shape_tf[3] == shape_py[0] and \
+                    shape_py[1] == 1:
+                gen_weights = np.zeros(shape_tf, dtype=np.float32)
+                py_transposed = weight_py.numpy().transpose(2, 3, 1, 0)
+                for ch in range(shape_tf[2]):
+                    gen_weights[:, :, ch, ch] = py_transposed[:, :, 0, ch]
+                weight_tf.assign(tf.convert_to_tensor(gen_weights))
+
+                if self.logger:
+                    self.logger.info(f'{name_tf} was assigned successfully from {name_py}, depthwise convolution')
+                return WeightHandlerReturn(processed=True, matched_source=True, matched_target=True)
+
+            return WeightHandlerReturn(processed=False, matched_source=False, matched_target=False)
+
+    class BatchNormalizationSkipExtraHandler(WeightHandler):
+        """Skips the 'num_batches_tracked' weight from pytorch BatchNorm layers."""
+
+        def _process_weights(self, source_weight, target_weight) -> WeightHandlerReturn:
+            _, name_py, _ = PytorchToTensorflowHandlers._preprocess_pytorch(source_weight)
+
+            if 'num_batches_tracked' in name_py:
+                if self.logger:
+                    self.logger.info(f'skipped {name_py}')
+                return WeightHandlerReturn(processed=True, matched_source=True, matched_target=False)
+
+            return WeightHandlerReturn(processed=False, matched_source=False, matched_target=False)
--- src/test/test_weights_converter.py
+++ src/test/test_weights_converter.py
@@ -1,7 +1,13 @@
 import logging
 from unittest import TestCase
 
+import numpy as np
+import tensorflow as tf
+import torch
+
 from model import weights_converter
+
+torch.set_grad_enabled(False)
 
 
 class TestWeightHandler(TestCase):
@@ -40,3 +46,99 @@ class TestWeightHandler(TestCase):
 
         result = handlers[0].convert_weight(None, None)
         self.assertEqual(result, processed_return)
+
+
+class TestPytorchToTensorflowHandlers(TestCase):
+    def test__preprocess_tensorflow(self):
+        name = 'a_random_name'
+        shape = (1, 1)
+        var = tf.Variable(name=name, shape=shape, initial_value=[[0]])
+        res_tensor, res_name, res_shape = weights_converter.PytorchToTensorflowHandlers._preprocess_tensorflow(var)
+        self.assertIs(var, res_tensor)
+        self.assertEqual(f'{name}:0', res_name)
+        self.assertEqual(shape, res_shape)
+
+    def test__preprocess_pytorch(self):
+        layer = torch.nn.Linear(1, 1, bias=False)
+        var = list(layer.state_dict().items())[0]
+        res_tensor, res_name, res_shape = weights_converter.PytorchToTensorflowHandlers._preprocess_pytorch(var)
+        self.assertIs(var[1], res_tensor)
+        self.assertIs(res_name, 'weight')
+        self.assertEqual(res_shape, (1, 1))
+
+
+class TestSameShapeHandler(TestCase):
+    def test(self):
+        inp = np.array([[1, 2, 3]], dtype=np.float32)
+        model_tensorflow = tf.keras.layers.BatchNormalization(momentum=0.1, epsilon=0.001)
+        model_tensorflow.trainable = False
+        model_tensorflow(inp)
+
+        model_pytorch = torch.nn.BatchNorm1d(3, momentum=0.1, eps=0.001)
+        model_pytorch.train(False)
+        model_pytorch.weight.uniform_()
+
+        converter = weights_converter.PytorchToTensorflowHandlers.SameShapeHandler()
+        res_weight = converter(('', model_pytorch.state_dict()['weight']), model_tensorflow.weights[0])
+        res_bias = converter(('', model_pytorch.state_dict()['bias']), model_tensorflow.weights[1])
+
+        np.testing.assert_allclose(model_tensorflow(inp).numpy(), model_pytorch(torch.tensor(inp)).numpy(), rtol=1e-5)
+        expected_res = weights_converter.WeightHandlerReturn(processed=True, matched_source=True, matched_target=True)
+        self.assertEqual(res_weight, expected_res)
+        self.assertEqual(res_bias, expected_res)
+
+
+class TestConvolutionHandler(TestCase):
+    def test(self):
+        in_channels = 3
+        kernel_size = 3
+        out_channels = 5
+        inp = np.ones((1, in_channels, 15, 15), dtype=np.float32)
+
+        model_tensorflow = tf.keras.layers.Conv2D(out_channels, kernel_size, use_bias=False,
+                                                  data_format='channels_first')
+        model_tensorflow(inp)
+
+        model_pytorch = torch.nn.Conv2d(in_channels, out_channels, kernel_size, bias=False)
+        model_pytorch.weight.uniform_()
+
+        converter = weights_converter.PytorchToTensorflowHandlers.ConvolutionHandler()
+        res = converter(('', model_pytorch.state_dict()['weight']), model_tensorflow.weights[0])
+
+        np.testing.assert_allclose(model_tensorflow(inp).numpy(), model_pytorch(torch.tensor(inp)).numpy(), rtol=1e-5)
+        expected_res = weights_converter.WeightHandlerReturn(processed=True, matched_source=True, matched_target=True)
+        self.assertEqual(res, expected_res)
+
+
+class TestDepthwiseTransposedConvolutionHandler(TestCase):
+    def test(self):
+        in_channels = 3
+        kernel_size = 3
+        out_channels = in_channels
+        inp = np.ones((1, in_channels, 15, 15), dtype=np.float32)
+
+        model_tensorflow = tf.keras.layers.Conv2DTranspose(out_channels, kernel_size, use_bias=False,
+                                                           data_format='channels_first')
+        model_tensorflow(inp)
+
+        model_pytorch = torch.nn.ConvTranspose2d(in_channels, out_channels, kernel_size, bias=False,
+                                                 groups=out_channels)
+        model_pytorch.weight.uniform_()
+
+        converter = weights_converter.PytorchToTensorflowHandlers.DepthwiseTransposedConvolutionHandler()
+        res = converter(('', model_pytorch.state_dict()['weight']), model_tensorflow.weights[0])
+
+        np.testing.assert_allclose(model_tensorflow(inp).numpy(), model_pytorch(torch.tensor(inp)).numpy(), rtol=1e-5)
+        expected_res = weights_converter.WeightHandlerReturn(processed=True, matched_source=True, matched_target=True)
+        self.assertEqual(res, expected_res)
+
+
+class TestBatchNormalizationSkipExtraHandler(TestCase):
+    def test(self):
+        model_pytorch = torch.nn.BatchNorm1d(3, momentum=0.1, eps=0.001)
+
+        converter = weights_converter.PytorchToTensorflowHandlers.BatchNormalizationSkipExtraHandler()
+        res = converter(list(model_pytorch.state_dict().items())[-1], None)
+
+        expected_res = weights_converter.WeightHandlerReturn(processed=True, matched_source=True, matched_target=False)
+        self.assertEqual(res, expected_res)
